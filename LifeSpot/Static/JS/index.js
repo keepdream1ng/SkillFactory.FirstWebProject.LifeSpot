c5c31c3f0c327de6699a0d82346a68c7d4a8a712
Anatolii Ivanov <gotnickname@gmail.com>
2023-09-09 15:43:26 +0600

    moved js to separate file

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,8 @@
+﻿let age;
+age = prompt("Пожалуйста, введите ваш возраст ниже:");
+if (age < 18) {
+    alert(`Контент сайта не предназначен для лиц младше 18!\nВы будете перенаправлены.`);
+    window.location.href = "http://www.google.com";
+    throw new Error("Not old enough for our content!");
+}
+alert(`Приветствуем на LiveSpot!\n Текущее время: ${new Date().toLocaleString()}`);
