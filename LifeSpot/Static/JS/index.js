4ca72a7f80b395912a52dd032360b3d885aed2fd
Anatolii Ivanov <gotnickname@gmail.com>
2023-09-18 09:26:17 +0530

    added map object for user

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,8 +1,17 @@
-﻿let age;
+﻿let sessionData = new Map();
+sessionData.set("UserAgent", window.navigator.userAgent);
+let age;
 age = prompt("Пожалуйста, введите ваш возраст ниже:");
+sessionData.set("UserAge", age);
 if (age < 18) {
     alert(`Контент сайта не предназначен для лиц младше 18!\nВы будете перенаправлены.`);
     window.location.href = "http://www.google.com";
     throw new Error("Not old enough for our content!");
 }
-alert(`Приветствуем на LiveSpot!\n Текущее время: ${new Date().toLocaleString()}`);
+let time = new Date().toLocaleString();
+sessionData.set("UserSessionStartTime", time);
+alert(`Приветствуем на LiveSpot!\n Текущее время: ${time}`);
+
+for (let prop of sessionData) {
+    console.log(prop);
+}
