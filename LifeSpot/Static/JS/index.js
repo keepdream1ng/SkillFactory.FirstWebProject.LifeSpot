83571f624b4a7a91c7f2f5f48d7d3276a17694f0
Anatolii Ivanov <gotnickname@gmail.com>
2023-09-18 14:50:08 +0530

    moved site opening code to separate function

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,19 +1,21 @@
-﻿let sessionData = new Map();
-sessionData.set("UserAgent", window.navigator.userAgent);
-let age;
-age = prompt("Пожалуйста, введите ваш возраст ниже:");
-sessionData.set("UserAge", age);
-if (age < 18) {
-    alert(`Контент сайта не предназначен для лиц младше 18!\nВы будете перенаправлены.`);
-    window.location.href = "http://www.google.com";
-    throw new Error("Not old enough for our content!");
-}
-let time = new Date().toLocaleString();
-sessionData.set("UserSessionStartTime", time);
-alert(`Приветствуем на LiveSpot!\n Текущее время: ${time}`);
+﻿function onSiteOpening() {
+    let sessionData = new Map();
+    sessionData.set("UserAgent", window.navigator.userAgent);
+    let age;
+    age = prompt("Пожалуйста, введите ваш возраст ниже:");
+    sessionData.set("UserAge", age);
+    if (age < 18) {
+        alert(`Контент сайта не предназначен для лиц младше 18!\nВы будете перенаправлены.`);
+        window.location.href = "http://www.google.com";
+        throw new Error("Not old enough for our content!");
+    }
+    let time = new Date().toLocaleString();
+    sessionData.set("UserSessionStartTime", time);
+    alert(`Приветствуем на LiveSpot!\n Текущее время: ${time}`);
 
-for (let prop of sessionData) {
-    console.log(prop);
+    for (let prop of sessionData) {
+        console.log(prop);
+    }
 }
 
 function filterVideoContainers() {
