b482c54b72ef856ecc56fbe5fca006ce4a26ad95
Anatolii Ivanov <gotnickname@gmail.com>
2023-09-19 06:44:15 +0530

    passing function practice

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,4 +1,9 @@
-﻿const onSiteOpening = function() {
+﻿const logSession = function (sessionData) {
+    for (let prop of sessionData) {
+        console.log(prop);
+    }
+}
+function HandleSession() {
     let sessionData = new Map();
     sessionData.set("UserAgent", window.navigator.userAgent);
     let age;
@@ -7,19 +12,20 @@
     if (age < 18) {
         alert(`Контент сайта не предназначен для лиц младше 18!\nВы будете перенаправлены.`);
         window.location.href = "http://www.google.com";
-        throw new Error("Not old enough for our content!");
+    } else {
+        let time = new Date().toLocaleString();
+        sessionData.set("UserSessionStartTime", time);
+        alert(`Приветствуем на LiveSpot!\n Текущее время: ${time}`);
     }
-    let time = new Date().toLocaleString();
-    sessionData.set("UserSessionStartTime", time);
-    alert(`Приветствуем на LiveSpot!\n Текущее время: ${time}`);
-
-    for (let prop of sessionData) {
-        console.log(prop);
-    }
+    return sessionData;
 }
 
-const filterVideoContainers = function() {
-    let userInput = document.getElementById('searchInput').value.toLowerCase();
+const getSearchInput = function() {
+    return document.getElementById('searchInput').value.toLowerCase();
+}
+
+function filterVideoContainers( getSearchInput ) {
+    const userInput = getSearchInput();
     let videoContainers = document.getElementsByClassName('video-container');
     for (let i = 0; i < videoContainers.length; i++) {
         let title = videoContainers[i].getElementsByClassName('video-title')[0];
@@ -29,6 +35,5 @@ const filterVideoContainers = function() {
         else {
             videoContainers[i].style.display = 'none';
         }
-        console.log(title.innerText);
     }
 }
