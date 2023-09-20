962ef186719ab2abbf74040c264521f98385c30e
Anatolii Ivanov <gotnickname@gmail.com>
2023-09-20 16:55:00 +0530

    added local comments

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,30 @@
+let commentForm = document.getElementById("commentForm");
+function submitComment(e) {
+    e.preventDefault();
+    addComment();
+}
+
+const showCommentForm = function () {
+    let formElements = Array.from(commentForm.children);
+    formElements.forEach(element => {
+        element.style.visibility = 'visible';
+    });
+}
+
+const writeComment = comment => {
+    document.getElementById("commentSection").innerHTML +=
+        `<div class="comment">\n`
+        + `<p><b>${comment['userName']}</b> ${comment['date']}</p>`
+        + `<p> ${comment['commentText']}</p>`
+        + `</div>`;
+}
+
+function addComment() {
+    let comment = {}
+    comment["userName"] = document.getElementById("commenterNameInput").value;
+    comment["commentText"] = document.getElementById("commentTextInput").value;
+    comment["date"] = new Date().toLocaleString();
+
+    writeComment(comment);
+}
+
