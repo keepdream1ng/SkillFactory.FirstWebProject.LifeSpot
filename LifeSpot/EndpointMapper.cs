a0c2e154fbf1ff3088390f966efd5d04eaafc3e6
Anatolii Ivanov <gotnickname@gmail.com>
2023-09-30 06:00:04 +0530

    moved mapping to separate class

---
+++ LifeSpot/EndpointMapper.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.IO;
+using System.Text;
+
+namespace LifeSpot
+{
+    public static class EndpointMapper
+    {
+        /// <summary>
+        ///  Маппинг CSS-файлов
+        /// </summary>
+        public static void MapCss(this IEndpointRouteBuilder builder)
+        {
+            var cssFiles = new[] { "index.css" };
+
+            foreach (var fileName in cssFiles)
+            {
+                builder.MapGet($"/Static/CSS/{fileName}", async context =>
+                {
+                    var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", fileName);
+                    var css = await File.ReadAllTextAsync(cssPath);
+                    await context.Response.WriteAsync(css);
+                });
+            }
+        }
+
+        /// <summary>
+        ///  Маппинг JS
+        /// </summary>
+        public static void MapJs(this IEndpointRouteBuilder builder)
+        {
+            var jsFiles = new[] {
+                "index.js",
+                "testing.js",
+                "about.js"
+            };
+
+            foreach (var fileName in jsFiles)
+            {
+                builder.MapGet($"/Static/JS/{fileName}", async context =>
+                {
+                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", fileName);
+                    var js = await File.ReadAllTextAsync(jsPath);
+                    await context.Response.WriteAsync(js);
+                });
+            }
+        }
+
+        /// <summary>
+        ///  Маппинг Html-страниц
+        /// </summary>
+        public static void MapHtml(this IEndpointRouteBuilder builder)
+        {
+            HtmlReplaceArgs replaceElements = new();
+            replaceElements.AddSharedElement("<!--SIDEBAR-->", "sidebar.html");
+            replaceElements.AddSharedElement("<!--FOOTER-->", "footer.html");
+
+            builder.MapGetView("/", "index.html", replaceElements);
+            builder.MapGetView("/testing", "testing.html", replaceElements);
+            builder.MapGetView("/about", "about.html", replaceElements);
+        }
+        private static void MapGetView(this IEndpointRouteBuilder builder, string webPath, string fileName, HtmlReplaceArgs replaceArgs = null)
+        {
+            builder.MapGet(webPath, async context =>
+            {
+                var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", fileName);
+                var viewText = await File.ReadAllTextAsync(viewPath);
+
+                // Загружаем шаблон страницы, вставляя в него элементы
+                var html = new StringBuilder(await File.ReadAllTextAsync(viewPath));
+                if ((replaceArgs != null)
+                    &&
+                    (replaceArgs.ReplacemantPairs.Count > 0))
+                {
+                    foreach (var item  in replaceArgs.ReplacemantPairs)
+                    {
+                        html.Replace(item.Key, item.Value);
+                    }
+                }
+
+                await context.Response.WriteAsync(html.ToString());
+            });
+
+        }
+    }
+}
---
+++ LifeSpot/HtmlReplaceArgs.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace LifeSpot
+{
+    public class HtmlReplaceArgs
+    {
+        public Dictionary<string, string> ReplacemantPairs = new();
+
+        public void AddSharedElement( string key, string fileName)
+        {
+            string htmlString = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", fileName));
+            ReplacemantPairs.Add(key, htmlString);
+        }
+    }
+}
